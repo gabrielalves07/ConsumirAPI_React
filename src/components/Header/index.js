584f2e8f4298a78bd8f5bfdf2f982a40a979df93
Gabriel Alves <85041753+gabrielalves07@users.noreply.github.com>
2023-05-26 16:32:11 -0300

    criando sistema de logout

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { FaHome, FaSignInAlt, FaUserAlt } from 'react-icons/fa';
+import {
+  FaHome,
+  FaSignInAlt,
+  FaUserAlt,
+  FaCircle,
+  FaPowerOff,
+} from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import * as actions from '../../store/modules/auth/actions';
+import history from '../../services/history';
 import { Nav } from './styled';
 
 export default function Header() {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
+  const dispatch = useDispatch();
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(actions.loginFailure());
+    toast.error('Você foi deslogado');
+    history.push('/');
+  };
+
   return (
     <Nav>
       <div>
@@ -14,9 +34,18 @@ export default function Header() {
         <Link to="/register" rel="noreferrer">
           <FaUserAlt size={24} />
         </Link>
-        <Link to="/login" rel="noreferrer">
-          <FaSignInAlt size={24} />
-        </Link>
+
+        {isLoggedIn ? (
+          <Link onClick={handleLogout} to="/login" rel="noreferrer">
+            <FaPowerOff size={24} />
+          </Link>
+        ) : (
+          <Link to="/login" rel="noreferrer">
+            <FaSignInAlt size={24} />
+          </Link>
+        )}
+
+        {isLoggedIn && <FaCircle fontSize={18} color="lightgreen" />}
       </div>
     </Nav>
   );
