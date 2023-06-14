ec758b449a0b392114d18b57a94789763c32883a
Gabriel Alves <85041753+gabrielalves07@users.noreply.github.com>
2023-06-14 18:04:05 -0300

    criando página para atualizar a foto do usuário

--- src/pages/Fotos/index.js
+++ src/pages/Fotos/index.js
@@ -1,11 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { get } from 'lodash';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
+import { toast } from 'react-toastify';
 import { Container } from '../../styles/globalStyles';
+import { Title, Form } from './styled';
+import axios from '../../services/axios';
+import history from '../../services/history';
+import * as actions from '../../store/modules/auth/actions';
 
-export default function Fotos() {
+import Loading from '../../components/Loading';
+
+export default function Fotos({ match }) {
+  const dispatch = useDispatch();
+  const id = get(match, 'params.id', '');
+
+  const [foto, setFoto] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  React.useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(`/alunos/${id}`);
+        setFoto(get(data, 'Fotos[0].url', ''));
+        setIsLoading(false);
+      } catch {
+        toast.error('Erro ao obter imagem');
+        setIsLoading(false);
+        history.push('/');
+      }
+    };
+
+    getData();
+  }, []);
+
+  const handleChange = async (e) => {
+    const fotoIcon = e.target.files[0];
+    const fotoURL = URL.createObjectURL(fotoIcon);
+
+    setFoto(fotoURL);
+
+    const formData = new FormData();
+    formData.append('aluno_id', id);
+    formData.append('foto', fotoIcon);
+
+    try {
+      setIsLoading(true);
+
+      await axios.post(`/foto/`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      toast.success('Foto atualizada com sucesso');
+
+      setIsLoading(false);
+    } catch (err) {
+      setIsLoading(false);
+      const { status } = get(err, 'response', '');
+
+      toast.error('Erro ao enviar foto');
+
+      if (status === 401) dispatch(actions.loginFailure());
+    }
+  };
+
   return (
     <Container>
-      <h1>Fotos</h1>
+      <Loading isLoading={isLoading} />
+
+      <Title>Fotos</Title>
+
+      <Form>
+        <label htmlFor="foto">
+          {foto ? <img src={foto} alt="Foto" /> : 'Selecionar'}
+          <input type="file" id="foto" onChange={handleChange} />
+        </label>
+      </Form>
     </Container>
   );
 }
+
+Fotos.propTypes = {
+  match: PropTypes.shape({}).isRequired,
+};
--- src/pages/Fotos/styled.js
+++ src/pages/Fotos/styled.js
@@ -1,3 +1,31 @@
 import styled from 'styled-components';
+import * as colors from '../../config/colors';
 
-export const Paragrafo = styled.p``;
+export const Title = styled.h1`
+  text-align: center;
+`;
+
+export const Form = styled.form`
+  label {
+    width: 160px;
+    height: 160px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #eee;
+    margin: 15px auto;
+    cursor: pointer;
+    border-radius: 50%;
+    border: 2px dashed ${colors.primaryColor};
+    overflow: hidden;
+
+    img {
+      width: 160px;
+      height: 160px;
+    }
+  }
+
+  input {
+    display: none;
+  }
+`;
